1e801d5eef68f0872fc3e6dcef49e6b61f8e68e2
Ashley Claiborne <languageisaliquid@gmail.com>
2019-09-23 19:51:54 -0500

    printed only book to dom

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -1,11 +1,28 @@
+const bookInfo = {
+    price: 24.99,
+    title: "Fifty Shades of Chicken",
+    image: "./assets/images/book.jpg"
+  };
+
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
 }
 
+const addToCartEvent = () => {
+    console.log('added to cart');
+};
+
 const makeStore = () => {
-    const domString = 'test new';
+    const domString = `
+        <h2>Our Only Book</h2>
+        <p>But it now:</p>
+        <h3>$${bookInfo.price}</h3>
+        <img src=${bookInfo.image} alt='${bookInfo.title} cover' />
+        <button id='cart-button' class='btn btn-danger col-12'>Add to cart</button>
+    `;
     printToDom('store-container', domString);
+    document.getElementById('cart-button').addEventListener('click', addToCartEvent);
 };
 
 export default { makeStore };
